8177d9e78b17453d6a7d535afb2d639f0c3af87c
Egor Frolikov <egorfrolikow@gmail.com>
2017-12-20 17:19:00 +0700

    сделал миксины для слайдеров, каждый слайдер независим(возможность вызвать несколько на одной странице)

--- src/modules/sliders/standartSlider/standartSliders.js
+++ src/modules/sliders/standartSlider/standartSliders.js
@@ -1,14 +1,20 @@
 
 $( function() {
-  var handle = $( ".standartSlider_custom-handle" );
-  $( ".standartSlider" ).slider({
-    value: 40,
-    range: "min",
-    create: function() {
-      handle.text( $( this ).slider( "value" ) );
-    },
-    slide: function( event, ui ) {
-      handle.text( ui.value );
-    }
-  });
-} );
+  $( ".standartSlider" ).each(function(){ //перебор всех элементов с данным классом
+    let handle = $(this).find( ".standartSlider_custom-handle" ); //запись в handle значения внутри каждого .standartSlider
+    $(this).slider({ //инициализация каждого слайдера взависимости от this
+      animate: true,
+      value: 40,
+      create: function() {
+        handle.text( $( this ).slider( "value" ) );
+      },
+      slide: function( event, ui ) {
+        handle.text( ui.value );
+      }
+    });
+  })
+} ); 
+
+
+
+
