fc3bd8f822b716423d7c1bdcf52d5abcf908a5e1
Egor Frolikov <egorfrolikow@gmail.com>
2017-12-27 17:28:35 +0700

    доделал блок с формой. настроил всплывающие подсказки

--- src/home.js
+++ src/home.js
@@ -4,5 +4,4 @@ import 'jquery-ui/ui/widgets/slider.js';
 import './modules/sliders/standartSlider/standartSliders';
 import './modules/sliders/stepSlider/stepSlider'
 
-
 import './style/base.styl';
