071cec770599a05de534ae05650291168c439da4
Egor Frolikov <egorfrolikow@gmail.com>
2018-03-13 23:52:58 +0700

    переделал логику чата, одна функция принимает объект со свойствами из других функций, убрал повторяемость кода. прикрепил недостающие иконки в чат. начал блок видеоплеера

--- src/modules/messager/messager.js
+++ src/modules/messager/messager.js
@@ -1,55 +1,51 @@
 'use strict'
 
 $(function () {
-  var date = new Date;
-  var options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
   var messageBase = [{
-    type: 'in', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', time: date.toLocaleString("ru", options)
+    type: 'in', text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
   }, {
-    type: 'out', text: 'A, maxime. Tenetur beatae delectus molestias.', time: date.toLocaleString("ru", options)
+    type: 'out', text: 'A, maxime. Tenetur beatae delectus molestias.'
   }, {
-    type: 'in', text: 'Quisquam quis,  pariatur, quam consequatur quos!', time: date.toLocaleString("ru", options)
+    type: 'in', text: 'Quisquam quis,  pariatur, quam consequatur quos!'
   }];
-  var messageText;
+  var message={};
 
-  function addMessage(type, text, time, newMessageClassName) {
+  function addMessage(message) {
+    var date = new Date;
+    var options = { hour: 'numeric', minute: 'numeric', second: 'numeric' };
     var messager = document.getElementsByClassName('messager__chat')[0];
     var newMessage = document.createElement('div');
-    if (type == "insertBaseMessage") {
-      newMessage.className = newMessageClassName;
-    }
-    else {
+    message.time = date.toLocaleString("ru", options);
+    if (message.type == 'out') {
       newMessage.className = 'messager__chat__message__out messager__chat__message';
     }
-    newMessage.innerHTML = text;
+    else {
+      newMessage.className = 'messager__chat__message__in messager__chat__message';
+    }
+    newMessage.innerHTML = message.text;
     messager.appendChild(newMessage);
-  }
+  };
 
   function loadMessageFromBase() {
     messageBase.forEach(function (item, i, messageBase) {
-      var newMessageClassName;
-      if (messageBase[i].type == 'out') {
-        newMessageClassName = 'messager__chat__message__out messager__chat__message';
-      }
-      else {
-        newMessageClassName = 'messager__chat__message__in messager__chat__message';
-      }
-      messageText = messageBase[i].text;
-      addMessage('insertBaseMessage', messageText, date.toLocaleString("ru", options), newMessageClassName);
+      message.type = messageBase[i].type;
+      message.text = messageBase[i].text;
+      addMessage(message);
     })
-  }
+  };
 
   function createOutMessage() {
-    messageText = document.getElementById('messager__input__newMessage').value;
-    if (messageText == "") {
+    message.type = 'out';
+    message.text= document.getElementById('messager__input__newMessage').value;
+    if (message.text == "") {
       return;
     }
     else {
-      addMessage('insertNewMessage', messageText, date.toLocaleString("ru", options));
+      addMessage(message);
       document.getElementById('messager__input__newMessage').value = '';
     }
-  }
+  };
   document.getElementById("messager__input__btn").addEventListener('click', createOutMessage);
 
   loadMessageFromBase(messageBase);
-})
+});
