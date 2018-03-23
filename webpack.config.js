4e47e7313d3eb0a63d1fa32e9df019eeaa8a3e06
Egor Frolikov <egorfrolikow@gmail.com>
2018-03-23 23:02:30 +0700

    изменил расположение всех изображений. инзменил конфиг вебпака. создал пробные файлы страничек.при сборке bundle не подключаются изображения

--- webpack.config.js
+++ webpack.config.js
@@ -4,40 +4,70 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-let pathToClean = 'dist';
+let pathToClean = 'public';
 
+const path = require('path');
 
 const common = {
   entry: "./src/home",
   output: {
-    path: __dirname + "/dist",
-    filename: "build.js"
+    path: path.resolve(__dirname, 'public'),
+    filename: "bundle/[name].js"
   },
 
   module: {
-    rules:
-    [
-      {
-        test: /\.styl$/,
-        use: ExtractTextPlugin.extract({
-          use: ['css-loader', 'stylus-loader']
-        })
-      }, {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        options: {
-          pretty: true
+    loaders:
+      [
+        {
+          test: /\.styl$/,
+          use: ExtractTextPlugin.extract({
+            use: ['css-loader', 'stylus-loader']
+          })
+        }, {
+          test: /\.pug$/,
+          loader: 'pug-loader',
+          options: {
+            pretty: true
+          }
+        }, {
+          test: /\.(ttf|eot|woff|woff2|png|jpg|jpeg|svg|gif)$/,
+          loader: 'file-loader',
+          include: '/images/',
         }
-      }, ]
+
+        // {
+        //   test: /\.(ttf|eot|woff|woff2|png|jpg|jpeg|svg|gif)$/,
+        //   loader: 'url-loader'
+        // }
+        // ,
+        // {
+        //   test: /\.(woff|woff2|eot|ttf)$/,
+        //   exclude: /node_modules/,
+        //   loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
+        // }
+      ]
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/base.pug",
+      inject: 'head',
+      filename: 'html/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      title: "firstPage(UIkit)",
+      filename: "pages/firstPage.html",
+      template: "src/pages/firstPage/firstPage.pug",
+      inject: 'head'
+    }),
+    new HtmlWebpackPlugin({
+      title: "blog(UIkit)",
+      filename: "pages/blog.html",
+      template: "src/pages/blog/blog.pug",
       inject: 'head'
     }),
     new CleanWebpackPlugin(pathToClean),
-    new ExtractTextPlugin('style.css'),
+    new ExtractTextPlugin('style/style.css'),
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery',
