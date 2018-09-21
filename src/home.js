8dd359f1473f349f1187d2316705c4d822841b2b
Egor Frolikov <egorfrolikow@gmail.com>
2018-09-21 15:55:19 +0700

    import #25

--- src/home.js
+++ src/home.js
@@ -1,19 +1,11 @@
-
 import './../node_modules/jquery-ui/ui/widgets/slider';
 import './../node_modules/jquery-ui/ui/widgets/datepicker';
 
-import './modules/buttons/standard-button/standard-button';
-
-import './modules/standard-slider/standard-slider';
-import './modules/step-slider/step-slider';
-import './modules/search/search';
-import './modules/search/jquery.highlight';
-import './modules/location/location';
-import './modules/calendar/calendar';
-import './modules/messager/messager';
-
 import './style/base.styl';
 
-import './pages/contacts/contacts';
-import './pages/catalog/product-info/product-info';
-import './pages/catalog/buy-item/buy-item';
+function importAll(r) {
+  r.keys().forEach(r);
+}
+
+importAll(require.context('./modules/', true, /\.js$/));
+importAll(require.context('./pages/', true, /\.js$/));
