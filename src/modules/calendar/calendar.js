ecb26f90ac5c099c41d5d229dde12e7462539df6
Egor Frolikov <egorfrolikow@gmail.com>
2018-10-08 17:37:09 +0700

    #36

--- src/modules/calendar/calendar.js
+++ src/modules/calendar/calendar.js
@@ -5,23 +5,28 @@ class Datepicker {
     this.$calendar = $(element);
     this.$day = this.$calendar.find('.js-calendar__day');
     this.$widget = this.$calendar.find('.js-calendar__widget');
-    this.$currentDay = this.$widget.find('.ui-datepicker-current');
+    this.$currentDay = this.$calendar.find('.js-calendar__today');
+    this.selectDay = new Date();
     this.initEvent();
   }
 
   initEvent() {
     this.$widget.datepicker({
-      showButtonPanel: true,
       showOtherMonths: true,
       changeYear: false,
       dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-      altField: this.$day,
-      altFormat: 'dd',
       firstDay: 1,
+      onSelect: () => {
+        this.selectDay = this.$widget.datepicker('getDate');
+        this.$day.text($.datepicker.formatDate('dd', this.selectDay));
+      },
     });
+
+    this.$day.text($.datepicker.formatDate('dd', this.selectDay));
 
     $(this.$currentDay).on('click', () => {
       this.$widget.datepicker('setDate', new Date());
+      this.$day.text($.datepicker.formatDate('dd', new Date()));
     });
   }
 }
