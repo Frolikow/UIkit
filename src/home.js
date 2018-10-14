35480edbacc6146ca58256ffd68d280a1e84d4c7
Egor Frolikov <egorfrolikow@gmail.com>
2018-10-15 00:37:35 +0700

    #43

--- src/home.js
+++ src/home.js
@@ -8,9 +8,9 @@ function importAll(r) {
   r.keys().forEach(r);
 }
 
-importAll(require.context('./modules/', true, /^\.\/.*\.styl$/));
-importAll(require.context('./pages/', true, /^\.\/.*\.styl$/));
-
 importAll(require.context('./favicons/', true, /^\.\/.*\.js$/));
 importAll(require.context('./modules/', true, /^\.\/.*\.js$/));
 importAll(require.context('./pages/', true, /^\.\/.*\.js$/));
+
+importAll(require.context('./modules/', true, /^\.\/.*\.styl$/));
+importAll(require.context('./pages/', true, /^\.\/.*\.styl$/));
