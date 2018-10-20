1c5b25654495bf4a6ca6770596237e2e7b9f5e86
Egor Frolikov <egorfrolikow@gmail.com>
2018-10-21 01:02:39 +0700

    #47. переписал заполнение шкалы в js

--- src/modules/step-slider/step-slider.js
+++ src/modules/step-slider/step-slider.js
@@ -1,17 +1,30 @@
+import $ from 'jquery';
+
 class StepSlider {
   constructor(element) {
     this.$slider = $(element);
+    this.value = +(this.$slider.data('default'));
+    this.min = +(this.$slider.data('minimum'));
+    this.max = +(this.$slider.data('maximum'));
+    this.step = +(this.$slider.data('step'));
     this.initEvent();
   }
 
   initEvent() {
     this.$slider.slider({
-      value: +(this.$slider.data('default')),
-      min: +(this.$slider.data('minimum')),
-      max: +(this.$slider.data('maximum')),
-      step: +(this.$slider.data('step')),
+      value: this.value,
+      min: this.min,
+      max: this.max,
+      step: this.step,
       range: 'min',
     });
+
+    const $scale = this.$slider.next();
+    let scaleValue = this.min;
+    while (scaleValue <= this.max) {
+      $scale.append(`<div class='step-slider__value'> ${scaleValue}</div>`);
+      scaleValue += this.step;
+    }
   }
 }
 
